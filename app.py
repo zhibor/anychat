59c62cc3c1f5b135ef809bcb16b75fcfc869d72d
AK <akhaliq@users.noreply.huggingface.co>
2024-11-17 01:58:57 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import gemini_gradio
 import openai_gradio
 import anthropic_gradio
 import sambanova_gradio
+import xai_gradio
 
 
 with gr.Blocks() as demo:
@@ -28,6 +29,12 @@ with gr.Blocks() as demo:
         gr.load(
         name='Meta-Llama-3.1-405B-Instruct',
         src=sambanova_gradio.registry,
+        accept_token=True
+        )
+    with gr.Tab("Grok"):
+        gr.load(
+        name='grok-beta',
+        src=xai_gradio.registry,
         accept_token=True
         )
 
