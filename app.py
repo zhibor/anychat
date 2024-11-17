c919a105079f0225601becf566bc465859193a80
AK <akhaliq@users.noreply.huggingface.co>
2024-11-17 01:08:11 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 import gradio as gr
 import gemini_gradio
+import openai_gradio
 
-gr.load(
-    name='gemini-1.5-pro-002',
-    src=gemini_gradio.registry,
-).launch()
+with gr.Blocks() as demo:
+    with gr.Tab("gemini-1.5-pro-002"):
+        gr.load(
+            name='gemini-1.5-pro-002',
+            src=gemini_gradio.registry,
+             accept_token=True
+        )
+    with gr.Tab("gpt-4-turbo"):
+        gr.load(
+        name='gpt-4-turbo',
+        src=openai_gradio.registry,
+        accept_token=True
+        )
+
+demo.launch()
+
+
