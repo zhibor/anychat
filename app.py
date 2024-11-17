af4bac74097fb7358de1abbe079e5ee6da282982
AK <akhaliq@users.noreply.huggingface.co>
2024-11-17 01:36:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import gemini_gradio
 import openai_gradio
 
 with gr.Blocks() as demo:
-    with gr.Tab("gemini-1.5-pro-002"):
+    with gr.Tab("Gemini"):
         gr.load(
             name='gemini-1.5-pro-002',
             src=gemini_gradio.registry,
              accept_token=True
         )
-    with gr.Tab("gpt-4-turbo"):
+    with gr.Tab("ChatGPT"):
         gr.load(
         name='gpt-4-turbo',
         src=openai_gradio.registry,
