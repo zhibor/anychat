cbe21ecabca62479cc3435623d73498266ac46e5
AK391 <ahsen@huggingface.co>
2024-11-21 09:39:31 -0500

    revert height changes

--- utils.py
+++ utils.py
@@ -13,18 +13,13 @@ def get_app(
     def update_model(new_model: str) -> list[gr.Column]:
         return [gr.Column(visible=model_name == new_model) for model_name in models]
 
-    chatbots = [gr.Chatbot(height=750) for _ in models]
-
     with gr.Blocks() as demo:
         model = gr.Dropdown(label="Select Model", choices=models, value=default_model)
 
         columns = []
-        for model_name, chatbot in zip(models, chatbots):
+        for model_name in models:
             with gr.Column(visible=model_name == default_model) as column:
-                if src != "models":
-                    gr.load(name=model_name, src=src, accept_token=accept_token, chatbot=chatbot, **kwargs)
-                else:
-                    gr.load(name=model_name, src=src, accept_token=accept_token, **kwargs)
+                gr.load(name=model_name, src=src, accept_token=accept_token, **kwargs)
             columns.append(column)
 
         model.change(
