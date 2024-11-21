517b735e359cf446a0b639daf33389af6076c86a
hysts <hysts@users.noreply.huggingface.co>
2024-11-21 08:02:24 +0000

    Temporary workaround of https://github.com/gradio-app/gradio/issues/9923

--- utils.py
+++ utils.py
@@ -13,13 +13,18 @@ def get_app(
     def update_model(new_model: str) -> list[gr.Column]:
         return [gr.Column(visible=model_name == new_model) for model_name in models]
 
+    chatbots = [gr.Chatbot(height=750) for _ in models]
+
     with gr.Blocks() as demo:
         model = gr.Dropdown(label="Select Model", choices=models, value=default_model)
 
         columns = []
-        for model_name in models:
+        for model_name, chatbot in zip(models, chatbots):
             with gr.Column(visible=model_name == default_model) as column:
-                gr.load(name=model_name, src=src, accept_token=accept_token, **kwargs)
+                if src != "models":
+                    gr.load(name=model_name, src=src, accept_token=accept_token, chatbot=chatbot, **kwargs)
+                else:
+                    gr.load(name=model_name, src=src, accept_token=accept_token, **kwargs)
             columns.append(column)
 
         model.change(
