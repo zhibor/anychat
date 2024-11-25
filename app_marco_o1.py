3f9cf44627ce6ec0124dc70417ca2f5bf7d413ef
AK391 <ahsen@huggingface.co>
2024-11-24 22:22:09 -0500

    zero-gpu

--- app_marco_o1.py
+++ app_marco_o1.py
@@ -1,16 +1,9 @@
-import os
-
+import gradio as gr
+import spaces
 import transformers_gradio
 
-from utils import get_app
-
-demo = get_app(
-    models=[
-        'AIDC-AI/Marco-o1'
-    ],
-    default_model="AIDC-AI/Marco-o1",
-    src=transformers_gradio.registry,
-)
+demo = gr.load(name="AIDC-AI/Marco-o1", src=transformers_gradio.registry)
+demo.fn = spaces.GPU()(demo.fn)
 
 if __name__ == "__main__":
     demo.launch()
