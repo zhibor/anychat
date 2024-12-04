0be3840e50da25d24d3de8ae8247a2811ffca24f
AK <akhaliq@users.noreply.huggingface.co>
2024-12-04 14:06:51 +0000

    Create app_sailor.py (#22)
    - Create app_sailor.py (a5809e5095a3fffa0ada6d288509004ed94f3b4c)
    
    
    Co-authored-by: Qian Liu <SivilTaram@users.noreply.huggingface.co>

---
+++ app_sailor.py
@@ -0,0 +1,12 @@
+import gradio as gr
+import spaces
+import transformers_gradio
+
+demo = gr.load(name="sail/Sailor2-20B-Chat", src=transformers_gradio.registry)
+demo.fn = spaces.GPU()(demo.fn)
+
+for fn in demo.fns.values():
+    fn.api_name = False
+
+if __name__ == "__main__":
+    demo.launch()
