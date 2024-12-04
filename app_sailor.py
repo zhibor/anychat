166d5a57a6ad15b66b5a9b28714d578f0358050d
AK391 <ahsen@huggingface.co>
2024-12-04 09:11:51 -0500

    update to use load

--- app_sailor.py
+++ app_sailor.py
@@ -1,9 +1,7 @@
 import gradio as gr
 import spaces
-import transformers_gradio
 
-demo = gr.load(name="sail/Sailor2-20B-Chat", src=transformers_gradio.registry)
-demo.fn = spaces.GPU()(demo.fn)
+demo = gr.load(name="sail/Sailor2-20B-Chat", src="spaces")
 
 for fn in demo.fns.values():
     fn.api_name = False
--- app_showui.py
+++ app_showui.py
@@ -1,12 +1,8 @@
 import gradio as gr
-import spaces
 
 # Load the Gradio space
 demo = gr.load(name="showlab/ShowUI", src="spaces")
 
-# Ensure the main function runs on a GPU
-if hasattr(demo, 'fn'):
-    demo.fn = spaces.GPU()(demo.fn)
 
 # Disable API access for all functions
 if hasattr(demo, 'fns'):
