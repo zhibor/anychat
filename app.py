d4fd01408b9195af72a2a2210a3fb3793e26c4f3
AK391 <ahsen@huggingface.co>
2024-12-09 21:43:04 -0800

    move gemini to first

--- app.py
+++ app.py
@@ -28,6 +28,9 @@ from app_showui import demo as demo_showui
 from app_omini import demo as demo_omini
 
 with gr.Blocks(fill_height=True) as demo:
+    with gr.Tab("Gemini"):
+        demo_gemini.render()
+        gr.Markdown("This app is built with gradio, check out gradio github and star: <a href='https://github.com/gradio-app/gradio'>Gradio <img src='https://img.shields.io/github/stars/gradio-app/gradio'></a>.")
     with gr.Tab("SambaNova (New Qwen 2.5 72B and Qwen 2.5 Coder 32B)"):
         demo_sambanova.render()
         gr.Markdown(
@@ -50,9 +53,6 @@ with gr.Blocks(fill_height=True) as demo:
         This app is built with gradio, check out gradio github and star: <a href='https://github.com/gradio-app/gradio'>Gradio <img src='https://img.shields.io/github/stars/gradio-app/gradio'></a>.
         """
         )
-    with gr.Tab("Gemini"):
-        demo_gemini.render()
-        gr.Markdown("This app is built with gradio, check out gradio github and star: <a href='https://github.com/gradio-app/gradio'>Gradio <img src='https://img.shields.io/github/stars/gradio-app/gradio'></a>.")
     with gr.Tab("OminiControl"):
         demo_omini.render()
         gr.Markdown("This app is built with gradio, check out gradio github and star: <a href='https://github.com/gradio-app/gradio'>Gradio <img src='https://img.shields.io/github/stars/gradio-app/gradio'></a>.")
